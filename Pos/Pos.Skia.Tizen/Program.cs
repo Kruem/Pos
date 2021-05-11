45cf7973160f8e195616fc359a4f998713223510
Kruem <lkgrayfox@gmail.com>
2021-05-10 23:28:36 -0500

    Initial Project whit Creation Db

---
+++ Pos/Pos.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace Pos.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new Pos.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ Pos/Pos.macOS/Main.cs
@@ -0,0 +1,15 @@
+﻿using AppKit;
+
+namespace Pos.macOS
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.SharedApplication.Delegate = new App();
+			NSApplication.Main(args);  
+		}
+	}
+}
+
